579dabaaa0d2872ecb1e38fd2dbc5500515052d7
Brett Gibson <bdg@brettdgibson.com>
2015-01-09 17:57:29 -0800

    get tests to run w/ Rails >= 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,10 @@ class Rails
   def self.root
     Pathname.new(File.join(File.dirname(__FILE__), '..'))
   end
+
+  def self.env
+    'test'
+  end
 end
 
 def test_path(glob)
@@ -14,7 +18,6 @@ end
 
 require 'active_support/concern'
 require 'active_record'
-require 'active_record/test_case'
 require 'active_record/fixtures'
 
 def create_fixtures(*table_names, &block)
@@ -26,7 +29,12 @@ require 'fixture_builder'
 
 class MagicalCreature < ActiveRecord::Base
   validates_presence_of :name, :species
-  default_scope :conditions => { :deleted => false }
+
+  if ActiveRecord::VERSION::MAJOR >= 4
+    default_scope -> { where(:deleted => false) }
+  else
+    default_scope :conditions => { :deleted => false }
+  end
 end
 
 def create_and_blow_away_old_db
